6ace63d4cf237daafcf59cdc3db3e00506c8eb55
APSS-Anon <135059171+APSS-Anon@users.noreply.github.com>
2025-07-21 11:44:46 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,15 @@ from os import path
 import sys
 sys.path.append(path.abspath('./UniReedSolomonm'))
 from UniReedSolomonm import rs
+from collections import Counter
+from OMapE.b4_oram import OMapE
 
 
 #num_cores = mp.cpu_count()
 num_cores = 32
+
+def column(matrix, i):
+   return [row[i] for row in matrix]
 
 def eq_matrix_one_col(dataset, s, vec_size):
     r = .85
@@ -46,53 +51,92 @@ def gen_eq_matrix_parallel(M, n, dataset, s, vec_size):
             eq_matrix[i][j] = col[i]
     return eq_matrix
 
+
+
+def process_column(j, row, lsh_list):
+    lsh_dict = {}
+    eq_mat_col = [0] * row
+    num_lsh_matches = 1
+
+    for i in range(1, row):
+        key = str(j) + ", " + str(lsh_list[i][j])
+        if key in lsh_dict:
+            eq_mat_col[i] = lsh_dict[key]
+            num_lsh_matches += 1
+        else:
+            lsh_dict[key] = i
+
+    return eq_mat_col, num_lsh_matches
+
+
 def gen_eq_matrix(M, n, lsh_list):
-    parallel = 0
     row, col = M, n
     eq_mat = [[0 for i in range(col)] for j in range(row)]
-
+    num_lsh_matches = [1 for i in range(col)]
     arr = []
 
     lsh_dict = {}
     for j in range(col):
-        # if j%100 == 0:
-        #     print("Current column is "+str(j))
         for i in range(1, row):
             key = str(j)+", "+str(lsh_list[i][j])
             if key in lsh_dict:
                 eq_mat[i][j] = lsh_dict[key]
+                num_lsh_matches[j] += 1
             else:
                 lsh_dict[key] = i
 
-    return eq_mat
+    return eq_mat, num_lsh_matches
+
 
 def is_valid_eq(eq_mat, k):
     flag = 0
     max_nonzero_count = []
+    nonzero_count_list = []
     nonzero_rows = 0
     failed_rows = 0
     max_nonzero_count.append(0)
     for i in range(len(eq_mat)):
+        flag_row = 0
         count_nonzero = 0
         for j in range(len(eq_mat[0])):
             if eq_mat[i][j] != 0:
                 count_nonzero += 1
+
                 if(count_nonzero > max_nonzero_count[nonzero_rows]):
                     max_nonzero_count[nonzero_rows] = count_nonzero
             if count_nonzero == k:
                 flag = 1
+                flag_row = 1
                 #break
         if count_nonzero != 0:
             nonzero_rows += 1
+            nonzero_count_list.append(count_nonzero)
             max_nonzero_count.append(0)
-        if flag == 1:
+        if flag_row == 1:
             #max_nonzero_count.append(k)
             failed_rows += 1
             #break
-    #print("Max nonzero count per row: " + str(max_nonzero_count))
+    # print("Max nonzero count per row: " + str(max_nonzero_count))
+    # print("nonzero count list: ", nonzero_count_list)
     print("Max nonzero count per matrix: " + str(max(max_nonzero_count)))
     print("Number of nonzero rows: " + str(nonzero_rows))
     print("Number of failed rows: " + str(failed_rows))
+
+    nonzero_count_list_col = []
+    # for j in range(len(eq_mat[0])):
+    #     # test_list = [0, 1, 1, 1, 0, 4, 4]
+    #     res = [counter for counter in Counter(column(eq_mat, j)).items()]
+        # print("Grouped and counted list is : ", res)
+        # print("Grouped and counted list is : ", [x[1] + 1 for x in res])
+
+    #     count_nonzero_col = 0
+    #     for i in range(len(eq_mat)):
+    #         if eq_mat[i][j] != 0:
+    #             count_nonzero_col += 1
+    #     if count_nonzero_col != 0:
+    #         nonzero_count_list_col.append(count_nonzero_col)
+    # print("nonzero count list per col: ", nonzero_count_list_col)
+
     return flag
 
 def sample_codes(n, k, M, eq):
@@ -116,20 +160,30 @@ def sample_codes(n, k, M, eq):
                 codes[i] += c[j]
     return codes
 
-def gen_dict(codes, M, n, lsh_list):
+def gen_dict(codes, M, n, lsh_list, map_type):
+
     dict = {}
     for i in range(M):
         for j in range(n):
             key = str(j) + ", " + str(lsh_list[i][j])
-            dict[key] = codes[i][j+1]
-    return dict
+            dict[key] = codes[i][j + 1]
+    if map_type == 'dict':
+        return dict
+    else:
+        omap = OMapE("./", total_accesses=1)
+        omap.apply(dict)
+        return omap
+
+
+
+
+
+
 
 def gen_map(codes, M, n, lsh_list):
     for i in range(M):
         for j in range(n):
             m.insert(chr(j)+lsh_list[i](j), codes[i](j))
-
-
 
 
 
