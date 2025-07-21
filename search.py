94f8b693bd583b316a24b0266dcf836f721a50b1
APSS-Anon <135059171+APSS-Anon@users.noreply.github.com>
2025-07-21 11:45:25 -0400

    Update search.py

--- search.py
+++ search.py
@@ -1,46 +1,37 @@
 # import unireedsolomon as rs
 from os import path
 import sys
+import OMapE
 sys.path.append(path.abspath('./UniReedSolomonm'))
 from UniReedSolomonm import rs
+import time
 
 #from reedsolo import RSCodec, ReedSolomonError
 
 from map import map as m
 
-# def search_query(l_query, lsh_list, k):
-#     n = len(lsh_list[0])
-#
-#     c = []
-#     erase = n
-#     for j in range(n):
-#         c[j] = m.retrieve(j, l_query(j))
-#         if c[j] is not None:
-#             erase -= 1
-#     if n-erase > k:
-#         return None
-#     codeword = ""
-#     for j in range(1, n):
-#         codeword += c[j]
-#     coder = rs.RSCoder(n, k)
-#     dec = coder.decode(codeword)
-#     print(dec[0])
-
-def search_query_dict(l_q, lsh_list, k, dict):
-    n = len(lsh_list[0])
+def search_query_dict(l_q, n, k, dict):
     c = [None for i in range(n+1)]
     erase = n
     erasure_pos = []
     erasure_pos.append(0)
+    parallel_time = 0
     for j in range(n):
-        if str(j) + ", " + str(l_q[j]) in dict:
-            c[j+1] = dict[str(j) + ", " + str(l_q[j])]
-        if c[j+1] is not None:
-            erase -= 1
-        else:
-            erasure_pos.append(j+1)
+        try:
+                t_start = time.time()
+                res = dict[str(j) + ", " + str(l_q[j])]
+                t_to_search = time.time() - t_start
+                if t_to_search> parallel_time:
+                    parallel_time = t_to_search
+                if  res is not None:
+                    c[j+1] = res
+                    erase -= 1
+                else:
+                    erasure_pos.append(j+1)
+        except KeyError:
+            erasure_pos.append(j + 1)
     if n - erase < k:
-        return "No match", erase
+        return "No match", erase, 0, parallel_time
     codeword = ""
     # print("c:", c)
     for char in c:
@@ -48,15 +39,22 @@ def search_query_dict(l_q, lsh_list, k, dict):
             codeword += char
         else:
             codeword += "\0"
-    # print("codeword: ", codeword)
-
     coder = rs.RSCoder(n+1, k)
     # coder = RSCodec(n)
-    dec = coder.decode_fast(codeword, erasures_pos=erasure_pos, only_erasures=False)
-    # print("dec", dec)
-    if dec != None:
-        return dec[0], erase
-    else:
-        return None, erase
-
+    try:
+        dec, ecc = coder.decode_fast(codeword, erasures_pos=erasure_pos, only_erasures=False,nostrip=True)
+        full_corrected = dec + ecc
+        errors=0
+        for i in range(n+1):
+            if i in erasure_pos:
+                continue
+            elif full_corrected[i] != codeword[i]:
+                errors = errors+1
+        dec, ecc = coder.decode_fast(codeword, erasures_pos=erasure_pos, only_erasures=False)
+        if dec != None:
+            return int(dec), erase, errors, parallel_time
+        else:
+            return 0, erase, errors, parallel_time
+    except (TypeError, ValueError):
+        return 0, 0, 0, parallel_time
 
